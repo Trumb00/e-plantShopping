7a3b73a91459b8323d8f1b0dc9a6e980c167b448
Trumb00 <57066818+Trumb00@users.noreply.github.com>
2025-03-04 11:03:11 +0100

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
+// Importing Necessary Functions and Files
+import { configureStore } from '@reduxjs/toolkit'; // Importing configureStore from @reduxjs/toolkit
+import cartReducer from './CartSlice'; // Importing the cartReducer from CartSlice.jsx
+
+// Configuring the Store
+const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer, // Assigning cartReducer to the cart slice of the state
     },
 });
-export default store
+
+// Exporting the Store
+export default store; // Exporting the configured store for use in the application
