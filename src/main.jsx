cd93eb21ac5da66883ae25d454bd2c52e5805003
Trumb00 <57066818+Trumb00@users.noreply.github.com>
2025-03-04 10:36:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux'; // 1. Importing Provider from react-redux
+import store from './store.js'; // 2. Importing the Redux store from store.js
+
+// 3. Wrapping the App component with the Provider component and passing the Redux store as a prop
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
+    <Provider store={store}>  {/* Redux store is passed here */}
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
